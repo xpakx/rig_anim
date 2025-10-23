fe4433329c5ff514a32b07b38cd951c2b2479d75
xpakx <55586470+xpakx@users.noreply.github.com>
2025-10-23 17:13:49 +0200

    feat: use matrices for bones

--- main.py
+++ main.py
@@ -14,36 +14,73 @@ set_target_fps(60)
 
 
 bones = [
-    {"name": "root", "length": 0, "rotation": 0, "x": 400, "y": 300},
-    {"name": "torso", "parent": "root", "length": 100, "rotation": -90},
-    {"name": "head", "parent": "torso", "length": 40, "rotation": 0},
-    {"name": "left_arm", "parent": "torso", "length": 60, "rotation": 45, "x": 23, "y": 15},
-    {"name": "right_arm", "parent": "torso", "length": 60, "rotation": -45, "x": -23, "y": 15},
-    {"name": "left_leg", "parent": "root", "length": 80, "rotation": 120, "x": -20},
-    {"name": "right_leg", "parent": "root", "length": 80, "rotation": 60, "x": 20},
+    {"name": "root", "length": 0, "rotation": 0, "x": 400, "y": 350},
+    {"name": "torso", "parent": "root", "length": 200, "rotation": -90},
+    {"name": "head", "parent": "torso", "length": 80, "rotation": 0},
+    {"name": "left_arm", "parent": "torso", "length": 120, "rotation": -45, "x": -30, "y": -46},
+    {"name": "right_arm", "parent": "torso", "length": 120, "rotation": 45, "x": -30, "y": 46},
+    {"name": "left_leg", "parent": "root", "length": 160, "rotation": 120, "x": -40},
+    {"name": "right_leg", "parent": "root", "length": 160, "rotation": 60, "x": 40},
 ]
 
 bones_dict = {b["name"]: b for b in bones}
 
-for b in bones:
+for bone in bones:
     try:
-        b["texture"] = load_texture(f"files/{b['name']}.png")
+        bone["texture"] = load_texture(f"files/{bone['name']}.png")
     except Exception:
-        b["texture"] = None
+        bone["texture"] = None
 
 
-def get_world_pos(bone):
-    if bone.get("parent"):
-        parent = bones_dict[bone["parent"]]
-        px, py, prot = get_world_pos(parent)
-        px += bone.get("x", 0)
-        py += bone.get("y", 0)
-        rad = math.radians(prot + bone["rotation"])
-        x = px + math.cos(rad) * bone["length"]
-        y = py + math.sin(rad) * bone["length"]
-        return x, y, prot + bone["rotation"]
-    else:
-        return bone.get("x", 0), bone.get("y", 0), bone.get("rotation", 0)
+def bone_local_matrix(bone):
+    rot = math.radians(bone.get("rotation", 0))
+    sx = bone.get("scaleX", 1)
+    sy = bone.get("scaleY", 1)
+    x = bone.get("x", 0)
+    y = bone.get("y", 0)
+
+    cosr = math.cos(rot)
+    sinr = math.sin(rot)
+
+    # a,b,c,d = rotation+scale
+    # tx,ty = local translation from parent
+    return [
+        cosr * sx, -sinr * sy,
+        sinr * sx,  cosr * sy,
+        x, y
+    ]
+
+
+def mul_mat2d(a, b):
+    return [
+        a[0]*b[0] + a[1]*b[2],  # a
+        a[0]*b[1] + a[1]*b[3],  # b
+        a[2]*b[0] + a[3]*b[2],  # c
+        a[2]*b[1] + a[3]*b[3],  # d
+        a[0]*b[4] + a[1]*b[5] + a[4],  # tx
+        a[2]*b[4] + a[3]*b[5] + a[5],  # ty
+    ]
+
+
+def tip_offset_matrix(parent):
+    tip = parent.get("length", 0)
+    return [
+        1, 0,
+        0, 1,
+        tip, 0
+    ]
+
+
+def bone_world_matrix(bone):
+    local = bone_local_matrix(bone)
+    parent_name = bone.get("parent")
+    if parent_name:
+        parent = bones_dict[parent_name]
+        parent_world = bone_world_matrix(parent)
+        tip_offset = tip_offset_matrix(parent)
+        parent_tip_world = mul_mat2d(parent_world, tip_offset)
+        return mul_mat2d(parent_tip_world, local)
+    return local
 
 
 angle = 0
@@ -53,44 +90,54 @@ while not window_should_close():
     begin_drawing()
     clear_background(RAYWHITE)
 
-    bones_dict["left_arm"]["rotation"] = 45 + math.sin(angle) * 30
-    bones_dict["right_arm"]["rotation"] = -45 - math.sin(angle) * 30
+    bones_dict["left_arm"]["rotation"] = -45 + math.sin(angle) * 30
+    bones_dict["right_arm"]["rotation"] = 45 - math.sin(angle) * 30
     bones_dict["left_leg"]["rotation"] = 120 - math.sin(angle) * 20
     bones_dict["right_leg"]["rotation"] = 60 + math.sin(angle) * 20
+    bones_dict["torso"]["rotation"] = -90 + math.sin(angle) * 5
     angle += 0.05
 
-    for b in bones:
-        if b.get("parent"):
-            px, py, _ = get_world_pos(bones_dict[b["parent"]])
-            px += b.get("x", 0)
-            py += b.get("y", 0)
-            x, y, _ = get_world_pos(b)
-            draw_line(int(px), int(py), int(x), int(y), BLACK)
-            draw_circle(int(x), int(y), 5, RED)
-            draw_circle(int(px), int(py), 5, RED)
-        else:
-            x, y, _ = get_world_pos(b)
-            draw_circle(int(x), int(y), 5, BLUE)
-        # TODO: attachments needs origin point defined to work better
-        if show_attachments and b.get("texture") and b.get("parent"):
-            offset = 90 if b['name'] in ['head', 'torso'] else -90
-            x, y, rot = get_world_pos(b)
-            px, py, _ = get_world_pos(bones_dict[b["parent"]])
-            px += b.get("x", 0)
-            py += b.get("y", 0)
-            tex = b["texture"]
-            scale_y = b["length"] / tex.height if tex.height != 0 else 1
-            if b['name'] in ['head', 'torso']:
-                px -= int(scale_y * tex.width/2)
-                py -= int(scale_y * tex.height)
-            else:
-                px += int(scale_y * tex.width/2)
-            draw_texture_ex(tex, Vector2(px, py), rot+offset, scale_y, RAYWHITE)
+    for bone in bones:
+        if bone.get("parent"):
+            wm = bone_world_matrix(bone)
+            x, y = wm[4], wm[5]
+            local_tip = (bone["length"], 0)
+            x_tip = wm[0] * local_tip[0] + wm[1] * local_tip[1] + wm[4]
+            y_tip = wm[2] * local_tip[0] + wm[3] * local_tip[1] + wm[5]
+
+            draw_line(int(x), int(y), int(x_tip), int(y_tip), BLACK)
+            draw_circle(int(x), int(y), 4, RED)
+            draw_circle(int(x_tip), int(y_tip), 4, RED)
+
+            # TODO: attachments needs origin point defined to work better
+            if show_attachments and bone.get("texture"):
+                tex = bone["texture"]
+                a, b, c, d, tx, ty = wm
+
+                px = a * b + tx
+                py = c * d + ty
+
+                rot = math.degrees(math.atan2(c, a)) - 90
+                scale_y = bone.get("length", 0) / tex.height if tex.height != 0 else 1
+                if bone['name'] in ['head', 'torso']:
+                    rot += 180
+                    py -= tex.height * scale_y
+                    px -= tex.width/2 * scale_y
+                elif bone['name'].endswith('leg'):
+                    px -= tex.width/2 * scale_y
+                elif bone['name'].endswith('arm'):
+                    px += tex.width/2 * scale_y
+
+                draw_texture_ex(tex, Vector2(px, py), rot, scale_y, RAYWHITE)
+
+    root = bones_dict["root"]
+    m = bone_local_matrix(root)
+    draw_circle(int(m[4]), int(m[5]), 5, BLUE)
 
     end_drawing()
 
-for b in bones:
-    if b.get("texture"):
-        unload_texture(b['texture'])
+for bone in bones:
+    if bone.get("texture"):
+        unload_texture(bone['texture'])
 
 close_window()
