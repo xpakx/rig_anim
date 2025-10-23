b46884928023849ee5ccd745634c737ca70d78ac
xpakx <55586470+xpakx@users.noreply.github.com>
2025-10-23 15:22:37 +0200

    feat: add very basic skin

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from raylib import (
         init_window, set_target_fps, window_should_close,
         begin_drawing, clear_background, draw_line,
         draw_circle, end_drawing, close_window,
+        load_texture, draw_texture_ex, unload_texture,
+        Vector2,
         RAYWHITE, BLACK, RED, BLUE
 )
 import math
@@ -12,7 +14,7 @@ set_target_fps(60)
 
 
 bones = [
-    {"name": "root", "parent": None, "length": 0, "rotation": 0, "x": 400, "y": 300},
+    {"name": "root", "length": 0, "rotation": 0, "x": 400, "y": 300},
     {"name": "torso", "parent": "root", "length": 100, "rotation": -90},
     {"name": "head", "parent": "torso", "length": 40, "rotation": 0},
     {"name": "left_arm", "parent": "torso", "length": 60, "rotation": 45},
@@ -23,9 +25,15 @@ bones = [
 
 bones_dict = {b["name"]: b for b in bones}
 
+for b in bones:
+    try:
+        b["texture"] = load_texture(f"files/{b['name']}.png")
+    except Exception:
+        b["texture"] = None
+
 
 def get_world_pos(bone):
-    if bone["parent"]:
+    if bone.get("parent"):
         parent = bones_dict[bone["parent"]]
         px, py, prot = get_world_pos(parent)
         rad = math.radians(prot + bone["rotation"])
@@ -37,6 +45,7 @@ def get_world_pos(bone):
 
 
 angle = 0
+show_attachments = False
 
 while not window_should_close():
     begin_drawing()
@@ -49,7 +58,7 @@ while not window_should_close():
     angle += 0.05
 
     for b in bones:
-        if b["parent"]:
+        if b.get("parent"):
             px, py, _ = get_world_pos(bones_dict[b["parent"]])
             x, y, _ = get_world_pos(b)
             draw_line(int(px), int(py), int(x), int(y), BLACK)
@@ -57,7 +66,24 @@ while not window_should_close():
         else:
             x, y, _ = get_world_pos(b)
             draw_circle(int(x), int(y), 5, BLUE)
+        # TODO: attachments needs origin point defined to work better
+        if show_attachments and b.get("texture") and b.get("parent"):
+            offset = 90 if b['name'] in ['head', 'torso'] else -90
+            x, y, rot = get_world_pos(b)
+            px, py, _ = get_world_pos(bones_dict[b["parent"]])
+            tex = b["texture"]
+            scale_y = b["length"] / tex.height if tex.height != 0 else 1
+            if b['name'] in ['head', 'torso']:
+                px -= int(scale_y * tex.width/2)
+                py -= int(scale_y * tex.height)
+            else:
+                px += int(scale_y * tex.width/2)
+            draw_texture_ex(tex, Vector2(px, py), rot+offset, scale_y, RAYWHITE)
 
     end_drawing()
+
+for b in bones:
+    if b.get("texture"):
+        unload_texture(b['texture'])
 
 close_window()
